843935e75a771a23d1208a2b22f35ed71ad90fc4
Artsuht <153464193+Artsuht@users.noreply.github.com>
2023-12-22 15:27:41 +0000

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -56,14 +56,5 @@ void Player::UpdatePosition(Map& map, int p_x, int p_y, int cur_x, int cur_y) //
 
 bool Player::EmptyTile(Map& map, int p_x, int p_y)
 {
-	if (map.GetMapAreaXY(p_x, p_y) == map.GetEmptyTile()) 
-	{
-		has_moved = true;//Functional but logical error. Function checks if player can move. Not if they have moved.
-		return true; 
-	}
-	else
-	{
-		has_moved = false;
-		return false;
-	}
+	return (map.GetMapAreaXY(p_x, p_y) == map.GetEmptyTile());
 }
