54a4dcf84d58670dddf0355a9408614b228ec861
Artsuht <153464193+Artsuht@users.noreply.github.com>
2023-12-23 12:50:01 +0000

    Update BaseEntities.h

--- BaseEntities.h
+++ BaseEntities.h
@@ -13,20 +13,20 @@ public:
 	BaseEntities(std::string ent_body) : entity_body(ent_body){}
 	void SpawnEntity(BaseEntities& entity, Map& map);
 	void GenerateEntity(Map& map, int quantity, int health, std::string ent_body);
-	//
+	
 	void MoveEntity(Map& map, Player& player);
 	void TriggerChase(BaseEntities& entity, Map& map, Player& player);
 	void ChasePlayer(BaseEntities& entity, Map& map, Player& player);
-	bool EmptyTile(BaseEntities& entity, int ent_y, int ent_x, Map& map);
-	int SpitRand(int min, int max); //Does what it says on the tin
-	//Getter
-	bool ChaseStatus() { return is_chasing_player; }
-	//Setter
 	void RandEntityXY(int min, int max) { entity_x = SpitRand(min, max); entity_y = SpitRand(min, max); }
+
+	int SpitRand(int min, int max); //Does what it says on the tin
+	bool EmptyTile(BaseEntities& entity, int ent_y, int ent_x, Map& map);
+	bool ChaseStatus() { return is_chasing_player; }
+	
 private:
 	std::vector<BaseEntities>entity_duplicates;
 	std::vector<std::string>inventory;
-        std::string entity_body;
+    std::string entity_body;
 
 	int entity_x = 0, entity_y = 0;
 	int entity_health = 0;
@@ -36,4 +36,3 @@ private:
 };
 
 #endif //BASEENTITIES_H
-
