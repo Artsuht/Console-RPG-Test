ab8c774fb907e186a666158fca7d33165c7456bf
Artsuht <153464193+Artsuht@users.noreply.github.com>
2023-12-23 13:34:58 +0000

    Update TerrainObject.cpp

--- TerrainObject.cpp
+++ TerrainObject.cpp
@@ -4,7 +4,7 @@ void TerrainObject::SpawnTerrainObject(TerrainObject& t_object, Map& map)
 {
 	while (map.GetMapAreaXY(t_object.t_object_x, t_object.t_object_x) != map.GetEmptyTile())
 	{
-		RandTObjXY(map.GetMapMinimum(), map.GetMapMaximum());
+		t_object.RandTObjXY(map.GetMapMinimum(), map.GetMapMaximum());
 	}
 	map.UpdateMap(t_object.t_object_x, t_object.t_object_y, t_object.t_object_tile);
 }
